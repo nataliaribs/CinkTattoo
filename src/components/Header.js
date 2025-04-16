e19a6b61bc72593086ddbb57d70eb970e2be89cc
nataliaribs <nlribeiro8@gmail.com>
2025-04-16 15:15:20 -0300

    Hamburguer C'ink

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,37 +1,45 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Header.css';
-import { FaInstagram } from 'react-icons/fa'; // Ícone do Instagram
 
 function Header() {
+  const [menuAberto, setMenuAberto] = useState(false);
+
+  const toggleMenu = () => {
+    setMenuAberto(!menuAberto);
+  };
+
   return (
     <header className="header">
-      <div className="logo">
-        <img
-          src={`${process.env.PUBLIC_URL}/imagem/logos/logoColorida.png`}
-          alt="Logo Estúdio Cink"
-        />
+      <div className="container-header">
+        <div className="logo">
+          <img
+            src={`${process.env.PUBLIC_URL}/imagem/logos/logoColorida.png`}
+            alt="Logo Estúdio Cink"
+          />
+        </div>
+
+        <button className="menu-toggle" onClick={toggleMenu}>
+          ☰
+        </button>
+
+        <nav className={`nav ${menuAberto ? 'ativo' : ''}`}>
+          <a href="#portfolio" onClick={toggleMenu}>Tatuadoras</a>
+          <a href="#agendamento" onClick={toggleMenu}>Agendamento</a>
+          <a href="#loja" onClick={toggleMenu}>Loja</a>
+          <a href="#cuidados" onClick={toggleMenu}>Cuidados</a>
+          <a href="#formulario" onClick={toggleMenu}>Formulário</a>
+          <a href="#localizacao" onClick={toggleMenu}>Localização</a>
+          <a
+            className="insta-link"
+            href="https://www.instagram.com/cink.tattoo"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <img
+            width="64" height="64" src="https://img.icons8.com/nolan/64/instagram-new.png" alt="instagram-new"/>
+          </a>
+        </nav>
       </div>
-
-      <nav className="nav">
-        <a href="#portfolio">Tatuadoras</a>
-        <a href="#agendamento">Agendamento</a>
-        <a href="#loja">Loja</a>
-        <a href="#cuidados">Cuidados</a>
-        <a href="#formulario">Formulário</a>
-        <a href="#localizacao">Localização</a>
-
-        {/* Ícone do Instagram */}
-        
-        <a
-  className="insta-link"
-  href="https://instagram.com/cink_estudio"
-  target="_blank"
-  rel="noopener noreferrer"
->
-<img width="50" height="50" src="https://img.icons8.com/ios/50/instagram-new--v1.png" alt="instagram-new--v1"/>
-</a>
-
-      </nav>
     </header>
   );
 }
